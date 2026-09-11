b745126179a6721ca6f9f2fb54d072ac28099597
Akash <akashkulkarni530@gmail.com>
2026-09-11 09:48:28 -0400

    chnages in app.tsx file

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -81,6 +81,7 @@ interface AuthCardProps {
 
 const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8080";
 
+
 // AUTH COMPONENT
 const AuthCard = ({ initialMode, onAuthSuccess }: AuthCardProps) => {
   const [mode, setMode] = useState<string>(initialMode);
